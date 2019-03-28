974d9ca9ec27bec2724fbd022b9d916403080b24
helena128 <hellogbel@gmail.com>
2019-03-28 08:56:44 +0300

    Implemented alpha_matrix creation

--- lab.py
+++ lab.py
@@ -1,6 +1,5 @@
 import sys
-
-nodes_matrix = []
+	
 filepath = './input.txt'
 
 def read_input():
@@ -41,24 +40,56 @@ def create_matrix(node_objects, node_dict):
 	return matrix
 
 def build_phis(node_marix):
+	phis = []
 	for row_idx in range(0, len(node_matrix)):
 		cur_row = node_matrix[row_idx]
-		cur_row_number = int(get_row_number(cur_row), 2)# print(cur_row_number)# TODO: watch overflow ?
-		row_zero_values = get_row_zero_values(cur_row)# print(row_zero_values)
-		last_row = 0
-		for zero_var in row_zero_values:
-   	 		print(zero_var)
-	return []
+		cur_row_number = int(get_row_number(cur_row), 2) # print(cur_row_number)# TODO: watch overflow ?
+		row_zero_values = get_row_zero_values(cur_row, row_idx) # print(row_zero_values)
+		concat_value = cur_row_number
+		cur_phi = [row_idx]
+		for zero_row in row_zero_values:
+   	 		zero_row_value = int(get_row_number(node_matrix[zero_row]), 2)
+   	 		if concat_value | zero_row_value > concat_value:
+   	 			concat_value = concat_value | zero_row_value
+   	 			#print('Primary row: ', row_idx, ' Zero row: ', zero_row, ' Value: ', '{0:b}'.format(concat_value))
+   	 			cur_phi.append(zero_row)
+   	 		if concat_value == 262143: # TODO: remove hardcoded value
+   	 			phis.append(cur_phi)
+   	 			#print(phis, 'Added: ', cur_phi)
+   	 			break
+	#print(phis)
+	return phis
 
 def get_row_number(cur_row):
     return ''.join(str(x) for x in cur_row)
 
-def get_row_zero_values(cur_row):
+def get_row_zero_values(cur_row, row_idx):
     zeroes = []
-    for idx in range(0, len(cur_row)):
+    for idx in range(row_idx, len(cur_row)):
     	if cur_row[idx] == 0:
     		zeroes.append(idx)
     return zeroes
+
+def get_phis_nodes(phis, node_dict):
+	phi_names = []
+	for node_row in phis:
+		cur_phi_row = []
+		for node in node_row:
+			cur_phi_row.append(next((val for val, cnt in node_dict.items() if cnt == node), None))
+		phi_names.append(phi_names)
+	return phi_names
+
+def create_alpha_matrix(phis):
+	alpha_matrix = [[0 for x in range(len(phis))] for y in range(len(phis))]
+	for row_idx1 in range(0, len(phis)):
+		phi_row1 = phis[row_idx1]
+		for row_idx2 in range(row_idx1 + 1, len(phis)):
+			phi_row2 = phis[row_idx2]
+			cur_alpha = len(phi_row1) + len(phi_row2) - len(list(set(phi_row1) & set(phi_row2)))
+			#print(cur_alpha)
+			alpha_matrix[row_idx1][row_idx2] = cur_alpha
+			alpha_matrix[row_idx2][row_idx1] = cur_alpha
+	return alpha_matrix
 
 if __name__ == '__main__':
     input_data = read_input()
@@ -66,6 +97,11 @@ if __name__ == '__main__':
     #print(node_objects)
     #print(node_dict)
     node_matrix = create_matrix(node_objects, node_dict)
-    for i in range(0, len(node_objects)):
+    for i in range(0, len(node_dict)):
     	print(node_matrix[i])
     phis = build_phis(node_matrix)
+    print('Phis\n', phis)
+    #phi_names = get_phis_nodes(phis, node_dict) #TODO
+    #print(phi_names)
+    alpha_matrix = create_alpha_matrix(phis)
+    print(alpha_matrix)
